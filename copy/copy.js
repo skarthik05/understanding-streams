4090db4981eb3d7d538c5b76526f01847f0242cb
karthik <karthiks0598@gmail.com>
2024-06-23 19:51:48 +0530

    feat(pipes): add .pipe() & pipeline() code

---
+++ copy/copy.js
@@ -0,0 +1,103 @@
+const { pipeline, finished } = require("node:stream");
+const fs = require("node:fs/promises");
+
+// File Size Copied: 1 GB
+// Memory Usage: 1 GB
+// Execution Time: 900 ms
+// Maximum File Size Able to Copy: 2 GB
+// (async () => {
+//   console.time("copy");
+//   const destFile = await fs.open("text-copy.txt", "w");
+//   const result = await fs.readFile("text-big.txt");
+
+//   await destFile.write(result);
+
+//   console.timeEnd("copy");
+// })();
+
+// File Size Copied: 1 GB
+// Memory Usage: 30 MB
+// Execution Time: 2 s
+// Maximum File Size Able to Copy: No Limit
+// (async () => {
+//   console.time("copy");
+
+//   const srcFile = await fs.open("text-small.txt", "r");
+//   const destFile = await fs.open("text-copy.txt", "w");
+
+//   let bytesRead = -1;
+
+//   while (bytesRead !== 0) {
+//     const readResult = await srcFile.read();
+//     bytesRead = readResult.bytesRead;
+//     console.log({ bytesRead }, { buffer: readResult.buffer[16380] });
+
+//     if (bytesRead != 16384) {
+//       const indexOfNotFilled = readResult.buffer.indexOf(0);
+//       console.log(indexOfNotFilled, readResult.buffer.length);
+//       const newBuffer = Buffer.alloc(indexOfNotFilled);
+//       readResult.buffer.copy(newBuffer, 0, 0, 16384);
+//       destFile.write(newBuffer);
+//     } else destFile.write(readResult.buffer);
+//   }
+
+//   console.timeEnd("copy");
+// })();
+
+// File Size Copied: 1 GB
+// Memory Usage: 30 MB
+// Execution Time: 1 s
+// Maximum File Size Able to Copy: No Limit
+// (async () => {
+//     console.time("copy");
+
+//     const srcFile = await fs.open("text-big.txt", "r");
+//     const destFile = await fs.open("text-copy.txt", "w");
+
+//     const readStream = srcFile.createReadStream();
+//     const writeStream = destFile.createWriteStream();
+
+// console.log(readStream.readableFlowing);
+
+// readStream.pipe(writeStream);
+
+// console.log(readStream.readableFlowing);
+
+// readStream.unpipe(writeStream);
+
+// console.log(readStream.readableFlowing);
+
+// readStream.pipe(writeStream);
+
+// console.log(readStream.readableFlowing);
+
+// readStream.on("end", () => {
+//   console.timeEnd("copy");
+// });
+
+// Don't use pipe in production, use pipeline instead! It will automatically
+// handle the cleanings for you and give you an easy way for error handling
+//     readStream.pipe(writeStream)
+//     readStream.on('end',()=>{
+
+//         console.timeEnd("copy");
+//     })
+//   })();
+
+// File Size Copied: 1 GB
+// Memory Usage: 30 MB
+// Execution Time: 1 s
+// Maximum File Size Able to Copy: No Limit
+(async () => {
+  console.time("copy");
+
+  const srcFile = await fs.open("text-big.txt", "r");
+  const destFile = await fs.open("text-copy.txt", "w");
+
+  const readStream = srcFile.createReadStream();
+  const writeStream = destFile.createWriteStream();
+  pipeline(readStream, writeStream, (err) => {
+    console.log(err);
+    console.timeEnd("copy");
+  });
+})();
